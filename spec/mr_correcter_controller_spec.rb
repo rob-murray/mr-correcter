19aa5bee146de447ae34d66eddda11696bf10451
Rob Murray <robmurray17@gmail.com>
2014-05-22 13:53:20 +0100

    Fix random failing specs; After changing the config, must reset it back so the next test is not affected.

--- spec/mr_correcter_controller_spec.rb
+++ spec/mr_correcter_controller_spec.rb
@@ -3,13 +3,10 @@ require 'spec_helper'
 describe MrCorrecter::MrCorrecterController do
 
   let(:logger) { stub_everything }
-
   let(:adapter) { mock }
-
   let(:correction_1) { MrCorrecter::Correction.new('tset', 'test') }
   let(:correction_2) { MrCorrecter::Correction.new('wrod', 'word') }
   let(:corrections) { [correction_1, correction_2] }
-
   let(:result_1) do
     Twitter::Tweet.new(
       id: 'tweet1',
@@ -26,13 +23,9 @@ describe MrCorrecter::MrCorrecterController do
       created_at: Time.now.to_s)
   end
   let(:results) { [result_1, result_2, result_3] }
-
   let(:search_period) { 10 }
 
   describe '#find_and_correct' do
-
-    let(:adapter) { mock }
-
     subject(:controller) { MrCorrecter::MrCorrecterController.new(adapter, logger, search_period) }
 
     before(:each) { adapter.stubs(:search).returns(results) }
@@ -44,16 +37,13 @@ describe MrCorrecter::MrCorrecterController do
     end
 
     context 'when post_tweets is enabled' do
-
       before do
         MrCorrecter.configure do |config|
           config.post_tweets = true
         end
       end
 
-      after do
-        MrCorrecter.reset
-      end
+      after { MrCorrecter.reset }
 
       it 'should reply to each tweet with a correcting tweet' do
         adapter.expects(:reply_to).with(result_1, anything)
@@ -62,31 +52,25 @@ describe MrCorrecter::MrCorrecterController do
 
         controller.find_and_correct([correction_1])
       end
-
     end
 
     context 'when post_tweets is disabled' do
-
       before do
         MrCorrecter.configure do |config|
           config.post_tweets = false
         end
       end
 
-      after do
-        MrCorrecter.reset
-      end
+      after { MrCorrecter.reset }
 
       it 'should reply to each tweet with a correcting tweet' do
         adapter.expects(:reply_to).never
 
         controller.find_and_correct([correction_1])
       end
-
     end
 
     context 'when Twitter API malfunctions' do
-
       before do
         MrCorrecter.configure do |config|
           config.post_tweets = true
@@ -109,25 +93,23 @@ describe MrCorrecter::MrCorrecterController do
 
         controller.find_and_correct([correction_1])
       end
-
     end
-
   end
 
   describe 'posting tweets in between posting replies' do
-
     subject(:controller) { MrCorrecter::MrCorrecterController.new(adapter, logger, search_period) }
 
     before(:each) { adapter.stubs(:search).returns(results) }
 
     context 'when post_tweets is enabled' do
-
       before do
         MrCorrecter.configure do |config|
           config.post_tweets = true
           config.new_tweet_post_interval = 2
         end
       end
+
+      after { MrCorrecter.reset }
 
       it 'should post a tweet at a regular interval' do
         adapter.expects(:reply_to).with(result_1, anything)
@@ -137,11 +119,9 @@ describe MrCorrecter::MrCorrecterController do
 
         controller.find_and_correct([correction_1])
       end
-
     end
 
     context 'when post_tweets is disabled' do
-
       before do
         MrCorrecter.configure do |config|
           config.post_tweets = false
@@ -149,14 +129,13 @@ describe MrCorrecter::MrCorrecterController do
         end
       end
 
+      after { MrCorrecter.reset }
+
       it 'should reply to each tweet with a correcting tweet' do
         adapter.expects(:send).never
 
         controller.find_and_correct([correction_1])
       end
-
     end
-
   end
-
 end
